a1146a5051a9638255b525bb75704b8c9c3af124
wakamantroy <troy_toendang@hotmail.com>
2018-10-30 13:26:36 +0100

    Hexagon

---
+++ Assets/Scripts/TileMapGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMapGenerator : MonoBehaviour
+{
+
+    public GameObject hexTilePrefab;
+
+    [SerializeField] int mapWidth = 25;
+    [SerializeField] int mapHeight = 12;
+
+    [SerializeField] float tileXOffset = 1.8f;
+    [SerializeField] float tileZOffset = 1.55f;
+
+    // Use this for initialization
+    void Update()
+    {
+        CreateHexTileMap();
+    }
+
+    // Update is called once per frame
+    void CreateHexTileMap()
+    {
+        for (int x = 0; x <= mapWidth; x++)
+        {
+            for (int z = 0; z <= mapHeight; z++)
+            {
+                GameObject TempGO = Instantiate(hexTilePrefab);
+
+                if (z % 2 == 0)
+                {
+                    TempGO.transform.position = new Vector3(x * tileXOffset, 0, z * tileZOffset);
+                }
+                else
+                {
+                    TempGO.transform.position = new Vector3(x * tileXOffset + tileXOffset / 2, 0, z * tileZOffset);
+                }
+            }
+        }
+    }
+
+    /*
+    IEnumerator LoadTiles(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            print("WaitAndPrint " + Time.time);
+        }
+    }
+    */
+}
